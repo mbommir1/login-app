30cce365140f87c7889c34a9f6404b14a7dcee6c
mbommir1 <mbommir1@asu.edu>
2017-09-22 11:36:41 -0700

    indentation and text changes

--- dbconnection.js
+++ dbconnection.js
@@ -14,7 +14,7 @@ var connectDB = function() {
   var con = mysql.createConnection({
     host: config.get('database.HOST'),
     user: config.get('database.USER_NAME'),
-  	password: config.get('database.PASSWORD'),
+    password: config.get('database.PASSWORD'),
     database: DB_NAME
   });
   return con;
@@ -23,8 +23,8 @@ var connectDB = function() {
 var connectSQL = function() {
   var con = mysql.createConnection({
     host: config.get('database.HOST'),
-  	user: config.get('database.USER_NAME'),
-  	password: config.get('database.PASSWORD')
+    user: config.get('database.USER_NAME'),
+    password: config.get('database.PASSWORD')
   });
   return con;
 };
@@ -39,41 +39,41 @@ var createDB = function(con) {
       if (err) throw err;
       console.log('Database created');
     });
-	 		
+
     con.query(`USE ${DB_NAME}`, function (err) {
       if (err) throw err;
       console.log('Database selected');
     });
-	 
+
     var sql = `CREATE TABLE IF NOT EXISTS ${TABLE_USERS} (user_name VARCHAR(255) NOT NULL UNIQUE, password CHAR(32) NOT NULL)`;
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log(`${TABLE_USERS} table created`);
-      
+
       var sql = `SELECT COUNT(*) as count FROM ${TABLE_USERS}`;
       con.query(sql, function (err, result) {
         if (err) throw err;
         if(result[0].count == 0) {
-            insertUser('aaa', md5('123'), function (err) {
-              if (err) {console.log(err + '- mas');}
-            });
-            insertUser('bbb', md5('123'), function (err) {
-              if (err) {console.log(err);}
-            });
+          insertUser('aaa', md5('123'), function (err) {
+            if (err) {console.log(err + '- mas');}
+          });
+          insertUser('bbb', md5('123'), function (err) {
+            if (err) {console.log(err);}
+          });
           insertUser('ccc', md5('123'), function (err) {
             if (err) {console.log(err);}
           });
         }
       });
-      
+
     });
-    
+
     var sql = `CREATE TABLE IF NOT EXISTS ${TABLE_LOGINS} (user_name VARCHAR(255) NOT NULL, time VARCHAR(255) NOT NULL)`;
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log(`${TABLE_LOGINS} table created`);
     });  
-    
+
     var sql = `CREATE TABLE IF NOT EXISTS ${TABLE_EVENTS} (user_name VARCHAR(255) NOT NULL, event_type VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, value VARCHAR(255), time VARCHAR(255) NOT NULL)`;
     con.query(sql, function (err, result) {
       if (err) throw err;
@@ -83,8 +83,8 @@ var createDB = function(con) {
 }
 
 exports.init = function () {
-	var con = connectDB();
-	createDB(con);
+  var con = connectDB();
+  createDB(con);
 };
 
 function insertUser(name, pass, callback) {
@@ -169,7 +169,7 @@ exports.addEventData = function (data, callback) {
     var time = helpers.getCurrentTime();
     var val = helpers.getFormattedAddInfo(data.value, data.element, data.event_type);
     var sql = `INSERT INTO ${TABLE_EVENTS} VALUES ('${data.user}', '${data.event_type}', '${data.url}', '${val}', '${time}')`;
-        
+
     con.query(sql, function (err, result) {
       if (err) throw err;
       callback(err);
--- server.js
+++ server.js
@@ -11,137 +11,137 @@ var jsonParser = bodyParser.json();
 
 exports.start = function() {
 
-	var app = express();
-	
-	app.use(express.static('public'));
-  
-    app.use(cookieParser());
-    
-    app.use(session({
-      secret: 'mbommir1',
-      resave: false,
-      saveUninitialized: true
-    }));
-	
-	app.set('view engine', 'pug');
-	
-	app.set('views','./views');
-	
-	app.get('/signup', function(req, res){
-      if(req.session.user)
-        res.redirect(302, '/');
-      else
-        res.render('signup');
-	});
-	
-	app.post('/signup', urlencodedParser, function(req, res) {
-      console.log('request for signup');
-      if(!req.body.user_name || !req.body.password) {
-        res.status('400');
-        res.render('signup', {message: 'Please enter both user name and password!'});
-      } else if (!helpers.isValidUserName(req.body.user_name)) {
-        res.status('400');
-        res.render('signup', {message: 'Only alphanumeric charaters are allowed in the username!'});
-      } else {
-        db.hasUser(req.body.user_name, function(err, count) {
-            if (count > 0) {
-              res.render('signup', {
-                message: 'User Already Exists! Login or choose another user name'
-              });
-            }
-            else {
-              db.addUser(req.body.user_name, md5(req.body.password), function (err, name) {
-                req.session.user = name;
-                res.cookie('user', name);
-                console.log('new user signed in');
-                db.addLoginInstance(name, function () {
-                  res.redirect(302, '/');
-                });
-              }); 
-            }
-        });
-      }
-	});
-  
-    app.post('/login', urlencodedParser, function(req, res) {
-      
-      if(!req.body.user_name || !req.body.password){
-        res.render('login', {message: 'Please enter both user name and password'});
-      } else {
-        db.getUser(req.body.user_name, function(err, user) {
-          if(user && user.user_name === req.body.user_name && user.password === md5(req.body.password)){
-            res.cookie('user', user.user_name);
-            req.session.user = user.user_name;
-            console.log('user logged in');
-            db.addLoginInstance(user.user_name, function () {
+  var app = express();
+
+  app.use(express.static('public'));
+
+  app.use(cookieParser());
+
+  app.use(session({
+    secret: 'mbommir1',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  app.set('view engine', 'pug');
+
+  app.set('views','./views');
+
+  app.get('/signup', function(req, res){
+    if(req.session.user)
+      res.redirect(302, '/');
+    else
+      res.render('signup');
+  });
+
+  app.post('/signup', urlencodedParser, function(req, res) {
+    console.log('request for signup');
+    if(!req.body.user_name || !req.body.password) {
+      res.status('400');
+      res.render('signup', {message: 'Please enter both user name and password!'});
+    } else if (!helpers.isValidUserName(req.body.user_name)) {
+      res.status('400');
+      res.render('signup', {message: 'Only alphanumeric charaters are allowed in the username!'});
+    } else {
+      db.hasUser(req.body.user_name, function(err, count) {
+        if (count > 0) {
+          res.render('signup', {
+            message: 'User Already Exists! Login or choose another user name'
+          });
+        }
+        else {
+          db.addUser(req.body.user_name, md5(req.body.password), function (err, name) {
+            req.session.user = name;
+            res.cookie('user', name);
+            console.log('new user signed in');
+            db.addLoginInstance(name, function () {
               res.redirect(302, '/');
             });
-          } else {
-            res.render('login', {message: 'Invalid credentials!'});
-          }
-        });
-      }
-	});
-  
-    app.get('/login', function (req, res) {
-      console.log('request for /login');
-      if(req.session.user) {
-        res.redirect(302, '/');
-      } else {
-        res.render('login');
-      }
-	});
-  
-    function checkLogIn(req, res, next) {
-      if(req.session.user) {
-        next();     //If session exists, proceed to page
-      } else {
-        res.redirect(302, '/login');
-      }
+          }); 
+        }
+      });
     }
-	
-	app.get('/', checkLogIn, function (req, res) {
-      var name = req.session.user;
-      var result = {};
-      db.getUserLoginHistory(name, function(err, loginData) {
-        result.login_data = loginData;
-        db.getUserEventHistory(name, function (err, eventData) {
-          result.event_data = eventData;
-          res.render('index', {name: name, result : result});
-        });
-      });
-	});
-  
-    app.get('/logout', function(req, res) {
-      console.log('request user log out');
-      req.session.destroy(function() { // destroy session
-        res.clearCookie('user'); // clear user name cookie
-        res.redirect(302, '/login');
-        console.log('user logged out');
-      });
-    });
-  
-    app.post('/eventlog', jsonParser, function(req, res) {
-      if (req.body.data && req.body.data.user) {
-        console.log('event log data received', req.body.data);
-        db.addEventData(req.body.data, function (err){
-          if (err) {
-            res.status('400');
-            res.send(err);
-          } else {
-            res.status('200');
-            res.send('Success!');
-          }
-        });
-      } else {
-        res.status('400');
-        res.send('Invalid request');
-      }
-    });
+  });
 
-	var server = app.listen(8081, function () {
-		var port = server.address().port
-		console.log(`server listening at ${port}`);
-	});
-	
+  app.post('/login', urlencodedParser, function(req, res) {
+
+    if(!req.body.user_name || !req.body.password){
+      res.render('login', {message: 'Please enter both user name and password'});
+    } else {
+      db.getUser(req.body.user_name, function(err, user) {
+        if(user && user.user_name === req.body.user_name && user.password === md5(req.body.password)){
+          res.cookie('user', user.user_name);
+          req.session.user = user.user_name;
+          console.log('user logged in');
+          db.addLoginInstance(user.user_name, function () {
+            res.redirect(302, '/');
+          });
+        } else {
+          res.render('login', {message: 'Invalid credentials!'});
+        }
+      });
+    }
+  });
+
+  app.get('/login', function (req, res) {
+    console.log('request for /login');
+    if(req.session.user) {
+      res.redirect(302, '/');
+    } else {
+      res.render('login');
+    }
+  });
+
+  function checkLogIn(req, res, next) {
+    if(req.session.user) {
+      next();     //If session exists, proceed to page
+    } else {
+      res.redirect(302, '/login');
+    }
+  }
+
+  app.get('/', checkLogIn, function (req, res) {
+    var name = req.session.user;
+    var result = {};
+    db.getUserLoginHistory(name, function(err, loginData) {
+      result.login_data = loginData;
+      db.getUserEventHistory(name, function (err, eventData) {
+        result.event_data = eventData;
+        res.render('index', {name: name, result : result});
+      });
+    });
+  });
+
+  app.get('/logout', function(req, res) {
+    console.log('request user log out');
+    req.session.destroy(function() { // destroy session
+      res.clearCookie('user'); // clear user name cookie
+      res.redirect(302, '/login');
+      console.log('user logged out');
+    });
+  });
+
+  app.post('/eventlog', jsonParser, function(req, res) {
+    if (req.body.data && req.body.data.user) {
+      console.log('event log data received', req.body.data);
+      db.addEventData(req.body.data, function (err){
+        if (err) {
+          res.status('400');
+          res.send(err);
+        } else {
+          res.status('200');
+          res.send('Success!');
+        }
+      });
+    } else {
+      res.status('400');
+      res.send('Invalid request');
+    }
+  });
+
+  var server = app.listen(8081, function () {
+    var port = server.address().port
+    console.log(`server listening at ${port}`);
+  });
+
 };
